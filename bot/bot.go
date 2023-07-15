f6149e6c6ecade850d02097cf1d7151e8223879c
amodotomi <amodotomi@gmail.com>
2023-07-15 21:52:49 +0300

    feat: mongo auth

--- bot/bot.go
+++ bot/bot.go
@@ -1,82 +1,35 @@
 package bot
 
 import (
-	"context"
 	"log"
-	"runtime/debug"
-	"time"
+  "os"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-type Bot struct {
-	api      *tgbotapi.BotAPI
-	cmdViews map[string]ViewFunc
-}
-
-func New(api *tgbotapi.BotAPI) *Bot {
-	return &Bot{api: api}
-}
-
-func (b *Bot) RegisterCmdView(cmd string, view ViewFunc) {
-	if b.cmdViews == nil {
-		b.cmdViews = make(map[string]ViewFunc)
+func bot() {
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TG_TOKEN"))
+	if err != nil {
+		log.Panic(err)
 	}
 
-	b.cmdViews[cmd] = view
-}
+	bot.Debug = true
 
-func (b *Bot) Run(ctx context.Context) error {
+	log.Printf("[SUCCESS] authorized on account %s", bot.Self.UserName)
+
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 
-	updates := b.api.GetUpdatesChan(u)
+	updates := bot.GetUpdatesChan(u)
 
-	for {
-		select {
-		case update := <-updates:
-			updateCtx, updateCancel := context.WithTimeout(context.Background(), 5*time.Minute)
-			b.handleUpdate(updateCtx, update)
-			updateCancel()
-		case <-ctx.Done():
-			return ctx.Err()
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+			msg.ReplyToMessageID = update.Message.MessageID
+
+			bot.Send(msg)
 		}
 	}
 }
-
-func (b *Bot) handleUpdate(ctx context.Context, update tgbotapi.Update) {
-	defer func() {
-		if p := recover(); p != nil {
-			log.Printf("[ERROR] panic recovered: %v\n%s", p, string(debug.Stack()))
-		}
-	}()
-
-	if (update.Message == nil || !update.Message.IsCommand()) && update.CallbackQuery == nil {
-		return
-	}
-
-	var view ViewFunc
-
-	if !update.Message.IsCommand() {
-		return
-	}
-
-	cmd := update.Message.Command()
-
-	cmdView, ok := b.cmdViews[cmd]
-	if !ok {
-		return
-	}
-
-	view = cmdView
-
-	if err := view(ctx, b.api, update); err != nil {
-		log.Printf("[ERROR] failed to execute view: %v", err)
-
-		if _, err := b.api.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Internal error")); err != nil {
-			log.Printf("[ERROR] failed to send error message: %v", err)
-		}
-	}
-}
-
-type ViewFunc func(ctx context.Context, bot *tgbotapi.BotAPI, update tgbotapi.Update) error
