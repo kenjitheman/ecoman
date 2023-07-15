83e65a4fb29aaf91f2a4fa8ad4f6b21ec777d9b1
amodotomi <amodotomi@gmail.com>
2023-07-15 20:08:49 +0300

    feat: data fetching, data formatting, tgbot api

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,29 @@
 package main
 
 import (
-  api "main.go/api"
-  "fmt"
+	"fmt"
+
+  "github.com/enescakir/emoji"
+	api "main.go/api"
 )
 
-func main()  {
-  api.FetchAndSaveData()
-  fmt.Println(api.GetCityDataFromMongoDB("Kyiv"))
+func main() {
+	// api.FetchAndSaveData()
+	cityData, err := api.GetCityDataFromMongoDB("Kyiv")
+	if err != nil {
+		fmt.Printf("[ERROR] error fetching city data from MongoDB: %v", err)
+	} else {
+		fmt.Println(emoji.Cityscape, "Місто:", cityData.CityName)
+		fmt.Println(emoji.House, "Вулиця:", cityData.StationName)
+  	fmt.Println(emoji.Compass, "Широта:", cityData.Latitude)
+		fmt.Println(emoji.Compass, "Довгота:", cityData.Longitude)
+    fmt.Println(emoji.ThreeOClock, "Часовий пояс:", cityData.Timezone)
+		for _, pollutant := range cityData.Pollutants {
+			fmt.Println(emoji.GemStone, "Показник:", pollutant.Pol)
+			fmt.Println("   + Одиниці: ", pollutant.Unit)
+			fmt.Println("   + Час:", pollutant.Time)
+			fmt.Println("   + Значення:", pollutant.Value)
+			fmt.Println("   + У середньому:", pollutant.Averaging)
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module main.go
 go 1.20
 
 require (
+	github.com/enescakir/emoji v1.0.0
 	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
 	go.mongodb.org/mongo-driver v1.12.0
 )
