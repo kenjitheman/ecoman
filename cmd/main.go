7eb49a82fc0fbcc3fcd25b0140796cf94ac4b0a0
amodotomi <amodotomi@gmail.com>
2023-07-23 15:41:33 +0300

    fix: double msg error

--- cmd/main.go
+++ cmd/main.go
@@ -2,39 +2,8 @@ package main
 
 import (
   "main.go/bot"
-	// ai "main.go/openai"
 )
 
 func main() {
 	bot.StartBot()
-// 	ai.GenerateAdvice(`🏙️ City: Berezhany
-// 🏠 Station: ploshcha Rynok
-// 🧭 Latitude: 49.445221
-// 🧭 Longitude: 24.938534
-// 🕒 Timezone: +0300
-// 💎 Pollutant: Humidity
-//    + Units: %
-//    + Time: 2023-07-23T14:41:41.000000Z
-//    + Value: 32
-//    + Average: 2 minutes
-// 💎 Pollutant: PM10
-//    + Units: ug/m3
-//    + Time: 2023-07-23T14:41:41.000000Z
-//    + Value: 5.8
-//    + Average: 2 minutes
-// 💎 Pollutant: PM2.5
-//    + Units: ug/m3
-//    + Time: 2023-07-23T14:41:41.000000Z
-//    + Value: 2.7
-//    + Average: 2 minutes
-// 💎 Pollutant: Temperature
-//    + Units: Celcius
-//    + Time: 2023-07-23T14:41:41.000000Z
-//    + Value: 30.57
-//    + Average: 2 minutes
-// 💎 Pollutant: Air Quality Index
-//    + Units: aqi
-//    + Time: 2023-07-23 14:00:00
-//    + Value: 14
-//    + Average: 1 hour`)
 }
