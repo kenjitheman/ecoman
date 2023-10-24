9d5ff7331eab905295c73cb1d07a289bfd70f050
kenjitheman <kenjitheman@proton.me>
2023-10-25 01:09:01 +0300

    test: db tests implemented

--- db/db_test.go
+++ db/db_test.go
@@ -1,1 +1,39 @@
 package db
+
+import (
+	"context"
+	"testing"
+)
+
+func TestGetMongoClient(t *testing.T) {
+	client, err := GetMongoClient()
+	if err != nil {
+		t.Errorf("Expected no error when obtaining MongoDB client, got: %v", err)
+	}
+	defer client.Disconnect(context.Background())
+
+	if client == nil {
+		t.Error("Expected a non-nil MongoDB client, got nil")
+	}
+}
+
+func TestSaveDataToMongoDB(t *testing.T) {
+	testCityData := CityData{
+		CityName:    "TestCity",
+		StationName: "TestStation",
+	}
+
+	err := SaveDataToMongoDB(testCityData)
+	if err != nil {
+		t.Errorf("Expected no error when saving data to MongoDB, got: %v", err)
+	}
+
+	retrievedCityData, err := GetCityData("TestCity", "TestStation")
+	if err != nil {
+		t.Errorf("Expected no error when retrieving saved data from MongoDB, got: %v", err)
+	}
+
+	if retrievedCityData.CityName != testCityData.CityName || retrievedCityData.StationName != testCityData.StationName {
+		t.Errorf("Saved data does not match retrieved data.")
+	}
+}
