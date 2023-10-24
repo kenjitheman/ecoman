798d64c82c86dffe7c252330078bc695a55b3af0
kenjitheman <kenjitheman@proton.me>
2023-10-25 01:06:57 +0300

    style: format openai_test.go

--- openai/openai_test.go
+++ openai/openai_test.go
@@ -5,9 +5,9 @@ import (
 )
 
 func TestGenerateAdvice(t *testing.T) {
-    result := "hello"
-    advice := GenerateAdvice(result)
-    if advice == "" {
-        t.Errorf("GenerateAdvice() failed, got: %s, want: %s.", advice, result)
-    }
+	result := "hello"
+	advice := GenerateAdvice(result)
+	if advice == "" {
+		t.Errorf("GenerateAdvice() failed, got: %s, want: %s.", advice, result)
+	}
 }
