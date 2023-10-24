1b55577096595de1d2934d28f4d098bbfe5a8b97
kenjitheman <kenjitheman@proton.me>
2023-10-25 00:44:23 +0300

    chore: vars(const) -> vars/vars.go

---
+++ vars/vars.go
@@ -0,0 +1,8 @@
+package vars
+
+const (
+	DataUrl        string = "https://api.saveecobot.com/output.json"
+	Prompt         string = " here is data, please, can you generate some advices what is best to do on this day, based on data you got"
+	DbName         string = "weatherdata"
+	CollectionName string = "dataman"
+)
