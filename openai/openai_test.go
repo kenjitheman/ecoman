5ee12d187b471df291adaf9cb7d354ef77f64335
kenjitheman <kenjitheman@proton.me>
2023-10-25 00:41:00 +0300

    test: tests init

---
+++ openai/openai_test.go
@@ -0,0 +1,13 @@
+package openai
+
+import (
+	"testing"
+)
+
+func TestGenerateAdvice(t *testing.T) {
+    result := "hello"
+    advice := GenerateAdvice(result)
+    if advice == "" {
+        t.Errorf("GenerateAdvice() failed, got: %s, want: %s.", advice, result)
+    }
+}
